06fe646a05b7cfa2abf7fce3470d6da23b4447a0
jakobob27 <jakob.huuse@gmail.com>
2023-03-30 19:27:23 +0200

    lol

--- src/main/java/com/ing/Player.java
+++ src/main/java/com/ing/Player.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import javafx.scene.Node;
 
-public class Player extends GameObject implements MapListener{
+public class Player extends GameObject implements MapListener {
 
     private boolean dead = false;
     private List<JumpscareListener> listeners = new ArrayList<>();
@@ -14,43 +14,38 @@ public class Player extends GameObject implements MapListener{
     public Player(Map map, int xpos, int ypos, Node id) {
         super(map, id);
         this.xpos = xpos;
-        this.ypos=ypos;
+        this.ypos = ypos;
     }
 
     @Override
     public void update(GameObject o) {
-            for (Animatronic animatronic: map.getAnimatronics()) {
-                if (animatronic.getPositionx()==xpos && animatronic.getPositiony()==ypos) {
-                    dead=true;
-                    System.out.println("YOU ARE DEAD!");
-                    for (Animatronic anima : map.getAnimatronics()) {
-                        anima.stopMoving();
-                    }
-                    
-                    for (JumpscareListener l: listeners) {
-                        l.jumpscare(animatronic);
-                    }
-
-                    break;
+        for (Animatronic animatronic : map.getAnimatronics()) {
+            if (animatronic.getPositionx() == xpos && animatronic.getPositiony() == ypos) {
+                dead = true;
+                System.out.println("YOU ARE DEAD!");
+                for (Animatronic anima : map.getAnimatronics()) {
+                    anima.stopMoving();
                 }
+
+                for (JumpscareListener l : listeners) {
+                    l.jumpscare(animatronic);
+                }
+
+                break;
             }
         }
-            
-    public void setDead (boolean dead) {
-        this.dead=dead;
+    }
+
+    public void setDead(boolean dead) {
+        this.dead = dead;
     }
 
     public boolean getDead() {
         return dead;
     }
 
-    public void addJumpscareListener (JumpscareListener listener) {
+    public void addJumpscareListener(JumpscareListener listener) {
         listeners.add(listener);
     }
 
-
-
 }
-
-    
-
