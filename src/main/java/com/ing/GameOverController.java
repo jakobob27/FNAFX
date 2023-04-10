4a85018018157435371063199bfd6a2211a24068
jakobob27 <jakob.huuse@gmail.com>
2023-04-10 16:54:45 +0200

    Added to module-info

--- src/main/java/com/ing/GameOverController.java
+++ src/main/java/com/ing/GameOverController.java
@@ -2,7 +2,6 @@ package com.ing;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.ing {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires transitive javafx.graphics;
 
     opens com.ing to javafx.fxml;
 
